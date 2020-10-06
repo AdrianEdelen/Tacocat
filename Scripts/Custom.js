84096a84bd762ce7d13713db08b299d83e590db7
Adrian Edelen <adrianedelen@gmail.com>
2020-10-06 16:42:41 -0400

    a lot of stuff

---
+++ Scripts/Custom.js
@@ -0,0 +1,16 @@
+﻿document.getElementById("reverseButton").addEventListener("click", () => {
+    let regex = /[\W_]/g;
+    let userInput = document.getElementById("userInput").value;
+    if (userInput === "") {
+        document.getElementById("result").innerText = "You gotta write something bud.";
+        return;
+    }
+    let userInputLowerReg = userInput.toLowerCase().replace(regex, '');
+    let revInput = userInputLowerReg.split('').reverse().join('');
+
+    if (revInput === userInputLowerReg) {
+        document.getElementById("result").innerText = userInput + " is a palindrome!";
+    } else {
+        document.getElementById("result").innerText = userInput + " is not a palindrome!";
+    }
+})
